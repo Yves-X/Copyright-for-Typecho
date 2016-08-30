b898d487dbf32d6f96c1c65b8b7132ad4363511e
Yves-X <Yves-X@foxmail.com>
2016-08-30 23:01:59 +0800

    Upload and README

---
+++ Plugin.php
@@ -0,0 +1,154 @@
+<?php
+
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+/**
+ * Copyright for Typecho
+ *
+ * @package Copyright
+ * @author  Yves X
+ * @version 0.9.0
+ * @link https://github.com/Yves-X/Copyright-for-Typecho
+ */
+
+class Copyright_Plugin implements Typecho_Plugin_Interface {
+	/**
+	 * 激活插件方法,如果激活失败,直接抛出异常
+	 * 
+	 * @access public
+	 * @return void
+	 * @throws Typecho_Plugin_Exception
+	 */
+    public static function activate() {
+    	Typecho_Plugin::factory('Widget_Abstract_Contents')->contentEx = array('Copyright_Plugin', 'copyright');
+    }
+
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     * 
+     * @static
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function deactivate(){}
+
+    /**
+     * 获取插件配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+    public static function config(Typecho_Widget_Helper_Form $form) {
+        echo "<p>此处为全局设置</p><p>你可以利用 Typecho 的自定义字段功能，对某篇文章或某个独立页面进行特定设置</p></p>特定设置的优先级始终高于默认设置</p>";
+        echo "<p>详细说明：<a href='https://github.com/Yves-X/Copyright-for-Typecho'>Github</a></p>";
+    	$author = new Typecho_Widget_Helper_Form_Element_Text('author', NULL, _t("作者名称"), _t('作者'));
+    	$form->addInput($author);
+    	$notice = new Typecho_Widget_Helper_Form_Element_Text('notice', NULL, _t("转载时须注明出处及本声明"), _t('声明'));
+    	$form->addInput($notice);
+        $showURL = new Typecho_Widget_Helper_Form_Element_Checkbox("showURL", array(1=>_t('显示原文链接')), NULL, NULL, NULL);
+        $form->addInput($showURL);
+        $showOnPost = new Typecho_Widget_Helper_Form_Element_Checkbox("showOnPost", array(1=>_t('在文章显示')), NULL, NULL, NULL);
+        $form->addInput($showOnPost);
+    	$showOnPage = new Typecho_Widget_Helper_Form_Element_Checkbox("showOnPage", array(1=>_t('在独立页面显示')), NULL, NULL, NULL);
+    	$form->addInput($showOnPage);
+    }
+
+    /**
+     * 个人用户的配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form
+     * @return void
+     */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+
+    /**
+     * 插件实现方法
+     * 
+     * @access public
+     * @return void
+     */
+    public static function copyright($content, $widget) {
+        $showOnPost = Typecho_Widget::widget('Widget_Options')->plugin('Copyright')->showOnPost;
+
+        if (! $widget->is('single')) {
+            echo $content;
+            return;
+        }
+
+        else if ($widget->parameter->type == "post") {
+        	if (isset($widget->fields->switch)) {
+        		if (!$widget->fields->switch) {
+        			echo $content;
+        		}
+        		else
+        			echo self::superAdd($content, $widget);
+        	}
+            else if ($showOnPost) {
+                echo self::superAdd($content, $widget);
+            }
+            else {
+                echo $content;
+            }
+
+        }
+
+        else if ($widget->parameter->type == "page") {
+        	if (isset($widget->fields->switch)) {
+        		if (!$widget->fields->switch) {
+        			echo $content;
+        		}
+        		else
+        			echo self::superAdd($content, $widget);
+        	}
+            else if ($showOnPage) {
+                echo self::superAdd($content, $widget);
+            }
+            else {
+                echo $content;
+            }
+        }
+
+        else {
+            echo $content;
+        }
+    }
+
+    private static function superAdd($content, $widget) {
+    	$res = "<hr /><blockquote>";
+    	if (isset($widget->fields->author)) {
+    		$author = $widget->fields->author;
+            if($author) {
+    		  $res = $res . "<p>版权属于：" . $author . "</p>";
+            }
+    	}
+    	else if ($author = Typecho_Widget::widget('Widget_Options')->plugin('Copyright')->author) {
+    		$res = $res . "<p>版权属于：" . $author . "</p>";
+    	}
+
+     	if (isset($widget->fields->url)) {
+    		$url = $widget->fields->url;
+            if($url) {
+    			$res = $res . "<p>原文链接：" . $url . "</p>";
+            }
+    	}
+    	else if ($showURL = Typecho_Widget::widget('Widget_Options')->plugin('Copyright')->showURL) {
+    		$url = $widget->permalink;
+    		$res = $res . "<p>本文链接：" . $url . "</p>";
+    	}
+
+    	if (isset($widget->fields->notice)) {
+    		$notice = $widget->fields->notice;
+            if($notice) {
+    		  $res = $res . "<p>" . $notice . "</p>";
+            }
+    	}
+    	else if ($notice = Typecho_Widget::widget('Widget_Options')->plugin('Copyright')->notice) {
+    		$res = $res . "<p>" . $notice . "</p>";
+    	}
+
+    	$res = $res . "</blockquote>";
+        return $content . $res;
+    }
+}
---
+++ README.md
@@ -0,0 +1,76 @@
+# Copyright for Typecho
+
+这是一个 [Typecho](https://github.com/typecho/typecho) 插件，利用自定义字段功能灵活地在文章或独立页面尾部显示版权小尾巴。
+
+Just like this:
+
+> 版权属于：Yves X
+> 原文链接： https://github.com/Yves-X/Copyright-for-Typecho
+> 转载时须注明出处及本声明
+
+## 安装
+
+0. 下载插件
+0. 打开 Typecho 根目录
+0. 解压至 ./usr/plugins/
+
+You can also:
+
+```bash
+# Initailly open the root document of your Typecho, then
+cd ./usr/plugins/
+git clone https://github.com/Yves-X/Copyright-for-Typecho.git
+```
+
+## 启用
+
+登入 Typecho 后台，控制台——插件——启用——设置
+
+## 设置
+
+你在插件管理中看到的设置项为**全局设置**，你也可以通过自定义字段，对单个文章/独立页面进行**特定设置**。
+
+当然，特定设置的优先级始终高于全局设置。
+
+你可以使用的字段有：
+
+| 字段 |类型|说明|
+|:---:|:---:|:----|
+|switch|整数|版权信息的开关（为0时关闭）|
+|author|字符|版权作者姓名（如：Yves X。为0时关闭）|
+|url|字符|原文链接（如：https://github.com/Yves-X/Copyright-for-Typecho。为0时关闭）|
+|notice|字符|版权声明（如：转载时须注明出处及本声明。为0时关闭）|
+
+注：上述所有数字值，都只在类型为“整数”时生效。
+
+## 示例
+
+假设你的全局设置如下：
+
+- 作者 Someone
+- 声明 转载时须注明出处及本声明
+- 显示原文链接 O
+- 在文章显示 X
+- 在独立页面显示 X
+
+显然，由于文章和独立页面的显示均关闭，插件并不会在任何地方输出版权信息。
+
+假设现在你需要对某篇特定的文章显示版权信息，有以下要求：
+
+0. 不显示作者
+0. 显示原文链接
+0. 声明为“禁止转载”
+
+你只需用自定义字段功能，将于全局设置不符的选项覆盖。在此处，你只需填写三条字段：
+
+| 字段 |类型|值|
+|:---:|:---:|:----:|
+|switch|整数|1|
+|author|整数|0|
+|notice|字符|禁止转载|
+
+显示效果为：
+
+> 原文链接：https://github.com/Yves-X/Copyright-for-Typecho
+
+> 禁止转载
