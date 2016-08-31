21b15bc9601ffee48b3bb26ebe464b5f0cd3294d
Yves-X <Yves-X@foxmail.com>
2016-08-31 16:25:10 +0800

    0.9.1
    - CSS 钩子
    - 链接
    - 有的没的代码风格

--- Plugin.php
+++ Plugin.php
@@ -6,7 +6,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  *
  * @package Copyright
  * @author  Yves X
- * @version 0.9.0
+ * @version 0.9.1
  * @link https://github.com/Yves-X/Copyright-for-Typecho
  */
 
@@ -40,17 +40,17 @@ class Copyright_Plugin implements Typecho_Plugin_Interface {
      * @return void
      */
     public static function config(Typecho_Widget_Helper_Form $form) {
-        echo "<p>此处为全局设置</p><p>你可以利用 Typecho 的自定义字段功能，对某篇文章或某个独立页面进行特定设置</p></p>特定设置的优先级始终高于默认设置</p>";
-        echo "<p>详细说明：<a href='https://github.com/Yves-X/Copyright-for-Typecho'>Github</a></p>";
-    	$author = new Typecho_Widget_Helper_Form_Element_Text('author', NULL, _t("作者名称"), _t('作者'));
+        echo '<p>此处为全局设置</p><p>你可以利用 Typecho 的自定义字段功能，对某篇文章或某个独立页面进行特定设置</p></p>特定设置的优先级始终高于默认设置</p>';
+        echo '<p>详细说明：<a href="https://github.com/Yves-X/Copyright-for-Typecho">Github</a></p>';
+    	$author = new Typecho_Widget_Helper_Form_Element_Text('author', NULL, _t('作者名称'), _t('作者'));
     	$form->addInput($author);
-    	$notice = new Typecho_Widget_Helper_Form_Element_Text('notice', NULL, _t("转载时须注明出处及本声明"), _t('声明'));
+    	$notice = new Typecho_Widget_Helper_Form_Element_Text('notice', NULL, _t('转载时须注明出处及本声明'), _t('声明'));
     	$form->addInput($notice);
-        $showURL = new Typecho_Widget_Helper_Form_Element_Checkbox("showURL", array(1=>_t('显示原文链接')), NULL, NULL, NULL);
+        $showURL = new Typecho_Widget_Helper_Form_Element_Checkbox('showURL', array(1=>_t('显示原文链接')), NULL, NULL, NULL);
         $form->addInput($showURL);
-        $showOnPost = new Typecho_Widget_Helper_Form_Element_Checkbox("showOnPost", array(1=>_t('在文章显示')), NULL, NULL, NULL);
+        $showOnPost = new Typecho_Widget_Helper_Form_Element_Checkbox('showOnPost', array(1=>_t('在文章显示')), NULL, NULL, NULL);
         $form->addInput($showOnPost);
-    	$showOnPage = new Typecho_Widget_Helper_Form_Element_Checkbox("showOnPage", array(1=>_t('在独立页面显示')), NULL, NULL, NULL);
+    	$showOnPage = new Typecho_Widget_Helper_Form_Element_Checkbox('showOnPage', array(1=>_t('在独立页面显示')), NULL, NULL, NULL);
     	$form->addInput($showOnPage);
     }
 
@@ -77,7 +77,7 @@ class Copyright_Plugin implements Typecho_Plugin_Interface {
             return;
         }
 
-        else if ($widget->parameter->type == "post") {
+        else if ($widget->parameter->type == 'post') {
         	if (isset($widget->fields->switch)) {
         		if (!$widget->fields->switch) {
         			echo $content;
@@ -94,7 +94,7 @@ class Copyright_Plugin implements Typecho_Plugin_Interface {
 
         }
 
-        else if ($widget->parameter->type == "page") {
+        else if ($widget->parameter->type == 'page') {
         	if (isset($widget->fields->switch)) {
         		if (!$widget->fields->switch) {
         			echo $content;
@@ -116,39 +116,39 @@ class Copyright_Plugin implements Typecho_Plugin_Interface {
     }
 
     private static function superAdd($content, $widget) {
-    	$res = "<hr /><blockquote>";
+    	$res = '<hr class="content-copyright" /><blockquote>';
     	if (isset($widget->fields->author)) {
     		$author = $widget->fields->author;
             if($author) {
-    		  $res = $res . "<p>版权属于：" . $author . "</p>";
+    		  $res = $res . '<p class="content-copyright">版权属于：' . $author . "</p>";
             }
     	}
     	else if ($author = Typecho_Widget::widget('Widget_Options')->plugin('Copyright')->author) {
-    		$res = $res . "<p>版权属于：" . $author . "</p>";
+    		$res = $res . '<p class="content-copyright">版权属于：' . $author . '</p>';
     	}
 
      	if (isset($widget->fields->url)) {
     		$url = $widget->fields->url;
             if($url) {
-    			$res = $res . "<p>原文链接：" . $url . "</p>";
+    			$res = $res . '<p class="content-copyright">原文链接：<a class="content-copyright" href="' . $url . '">' . $url . '</a></p>';
             }
     	}
     	else if ($showURL = Typecho_Widget::widget('Widget_Options')->plugin('Copyright')->showURL) {
     		$url = $widget->permalink;
-    		$res = $res . "<p>本文链接：" . $url . "</p>";
+    		$res = $res . '<p class="content-copyright">本文链接：<a class="content-copyright" href="' . $url . '">' . $url . '</a></p>';
     	}
 
     	if (isset($widget->fields->notice)) {
     		$notice = $widget->fields->notice;
             if($notice) {
-    		  $res = $res . "<p>" . $notice . "</p>";
+    		  $res = $res . '<p class="content-copyright">' . $notice .'</p>';
             }
     	}
     	else if ($notice = Typecho_Widget::widget('Widget_Options')->plugin('Copyright')->notice) {
-    		$res = $res . "<p>" . $notice . "</p>";
+    		$res = $res . '<p class="content-copyright">' . $notice . '</p>';
     	}
 
-    	$res = $res . "</blockquote>";
+    	$res = $res . '</blockquote>';
         return $content . $res;
     }
 }
